c3f3a1f57bfe14e121a5f00e4b4acd05be8f07ca
yabuleo <leoyabuzaki29@gmail.com>
2023-04-30 16:13:00 +0900

    commit

--- src/allowlist.js
+++ src/allowlist.js
@@ -1,4 +1,6 @@
 export const allowlistAddresses = [
   ["0xdecf4b112d4120b6998e5020a6b4819e490f7db6",5],
   ["0x3f4b74bc939aca43627ba236725a431add817f59",5],
+  ["0x51bb22a4e9B283d1fa25c829293466e47284e32f",5],
+  ["0x51bB22A4E9B283d1fa25C829293466E47284E32f",5],
 ]
